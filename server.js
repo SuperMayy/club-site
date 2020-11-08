af107b1e9b35ea7ab654ec4225550093e5917556
SuperMayy <mayowashowumi@outlook.com>
2020-11-08 01:53:12 +0000

    Serverd the index.html from express server

--- server.js
+++ server.js
@@ -40,6 +40,12 @@ app.use('/api', routes);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 app.listen(PORT, console.log(`Server is running on ${PORT}`));
